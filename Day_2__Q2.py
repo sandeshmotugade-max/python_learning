d462c1f3e8378a36c7fd3133bcc0b0e55a707060
SANDESH <sandeshmotugade@gmail.com>
2026-02-15 09:47:37 +0530

    day 2

---
+++ Day_2__Q2.py
@@ -0,0 +1,24 @@
+# print your bank accunt details
+
+Account_holder_name = "Sandesh Savanta Motugade"
+Accunt_number = 10059465185
+IFSC_code = "BARBOMIRAJX"
+Branch_name = "Bank 0f Baroda miraj"
+Account_Blance = 5555.05
+Intrest_rate = 6.10
+
+print("----------------Memory Refence Identification-----------------")
+print("Account_holder_name = ",id(Account_holder_name))
+print("Accunt_number = ",id(Accunt_number))
+print("IFSC_code = ",id(IFSC_code))
+print("Branch_name = ",id(Branch_name))
+print("Account_Blance = ",id(Account_Blance))
+print("Intrest_rate = ",id(Intrest_rate))
+
+print("----------------Type Identification-----------------")
+print("Account_holder_name = ",type(Account_holder_name))
+print("Accunt_number = ",type(Accunt_number))
+print("IFSC_code = ",type(IFSC_code))
+print("Branch_name = ",type(Branch_name))
+print("Account_Blance = ",type(Account_Blance))
+print("Intrest_rate = ",type(Intrest_rate))
