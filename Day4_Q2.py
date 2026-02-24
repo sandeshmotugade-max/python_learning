2302317faf34b19fff20ff0bcf2279bf28b9f315
SANDESH <sandeshmotugade@gmail.com>
2026-02-24 13:56:34 +0530

    movie_list

---
+++ Day4_Q2.py
@@ -0,0 +1,16 @@
+
+movies_list = ["Border", "3 Idiots", "Bahubali", "Chava", "KGF"]
+
+print("First movie:", movies_list[0])
+print("Last movie:", movies_list[-1])
+print("")
+
+movies_list[1] = "Lio"
+print(movies_list)
+print("")
+
+movies_list.sort()
+print(movies_list)
+print("")
+
+print("Total movies_list:", len(movies_list))
